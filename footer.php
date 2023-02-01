8041bf073053b18a3fc6716c4fadc7c30a0108f5
Jefffvvv <105692008+Jefffvvv@users.noreply.github.com>
2023-02-01 19:46:55 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
 <div class="fixed-bottom">
-        <footer class="p-3 bg-dark text-light d-flex justify-content-center"><p>Copyright &copy; <b><a href="https://www.instagram.com/jefffvvv/">@_
+        <footer class="p-3 bg-dark text-light d-flex justify-content-center"><p>Copyright &copy; <b><a href="https://www.instagram.com/jefffvvv/">@
 jefffvvv</a></b> All right reserved.</p></footer>
     </div>
